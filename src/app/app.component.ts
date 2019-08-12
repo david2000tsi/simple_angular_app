8587a0502d57d630c888dc881b3e3b3d37346ac7
David Alves <david2000tsi@gmail.com>
2019-08-12 11:20:00 -0300

    Added two basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { MySecondComponentComponent } from './my-second-component/my-second-component.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'simple-angular-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { MySecondComponentComponent } from './my-second-component/my-second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent,
+    MySecondComponentComponent
   ],
   imports: [
     BrowserModule
